836220d224bd9d4ce63f3787f10b7f846d7c1e34
Iroshan Brian Bandaragoda <brianstudiolk@gmail.com>
2024-05-25 18:30:11 +0530

    #Added poppins default font to tailwind conf and imported the GF to the index.css

--- Project Vite + Tailwind/projectViteTailwind.ps1
+++ Project Vite + Tailwind/projectViteTailwind.ps1
@@ -45,17 +45,22 @@ Write-Host "[SUCCESS] Tailwind Installed Successfully!" -ForegroundColor Green
 # Configure Tailwind
 $tailwindConfigPath = "tailwind.config.js"
 $tailwindConfigContent = @'
-/** @type {import("tailwindcss").Config} */
+/** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        title: ['Poppins'],
+      }
+    },
   },
-  plugins: [],
-}
+  plugins: [
+  ],
+};
 '@
 
 Set-Content -Path $tailwindConfigPath -Value $tailwindConfigContent
@@ -73,9 +78,17 @@ if (Test-Path -Path $appCssPath) {
 # Add Tailwind directives to index.css
 $indexCssPath = "src/index.css"
 $indexCssContent = @'
+@import url('https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap');
+
 @tailwind base;
 @tailwind components;
 @tailwind utilities;
+
+* {
+     scroll-behavior: smooth;
+     box-sizing: border-box;
+     margin: 0;
+}
 '@
 
 Set-Content -Path $indexCssPath -Value $indexCssContent
